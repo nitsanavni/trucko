099d69926cf1e1cc6ceae6508b768f17e7e5570e
Nitsan Avni <nitsanav@gmail.com>
2015-09-09 22:19:41 +0300

    use assertj

--- truckolib/src/test/java/com/meirco/truckolib/TruckOTest.java
+++ truckolib/src/test/java/com/meirco/truckolib/TruckOTest.java
@@ -1,10 +1,12 @@
 package com.meirco.truckolib;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import junit.framework.TestCase;
 
-public class TruckOTest extends TestCase{
+public class TruckOTest extends TestCase {
     public void testGetString() {
         String actual = TruckO.getInstance().getString();
-        assertEquals("hello", actual);
+        assertThat(actual).isEqualTo("TruckO lib o yeah!");
     }
 }
